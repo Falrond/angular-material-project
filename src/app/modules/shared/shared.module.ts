45724004380d1086f2b855ca2a815d4f5405cde9
Michał Winiecki <falrond@gmail.com>
2024-01-07 13:26:47 +0100

    first commit

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlertComponent } from './components/alert/alert.component';
+@NgModule({
+  declarations: [AlertComponent],
+  imports: [CommonModule],
+  exports: [
+    CommonModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AlertComponent,
+  ],
+})
+export class SharedModule {}
